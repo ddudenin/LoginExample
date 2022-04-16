a749a4bee86682ab16eddce1dd5d6971d92b01fb
Dmitry <dmitry_dudenin@mail.ru>
2022-04-16 22:19:00 +0300

    init commit

---
+++ CombineWithSwiftUI/CombineWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineWithSwiftUIApp.swift
+//  CombineWithSwiftUI
+//
+//  Created by Дмитрий Дуденин on 16.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct CombineWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CombineWithSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CombineWithSwiftUI
+//
+//  Created by Дмитрий Дуденин on 16.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
